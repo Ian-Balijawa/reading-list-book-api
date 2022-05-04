561e8c54b0026ff2a70204cd8d4f963728ac4db1
ian <ianbalijawa16@gmail.com>
2022-05-04 16:06:43 +0300

    create mutation to delete an author

---
+++ src/mutations/author/delete.ts
@@ -0,0 +1,20 @@
+import Author, { AuthorDoc } from '../../models/Author';
+import Book from '../../models/Book';
+
+type Args = {
+    authorId: string;
+};
+
+const deleteAuthorMutation = async (
+	parent: unknown,
+	args: Args
+): Promise<AuthorDoc> => {
+	let deletedAuthor = await Author.findByIdAndRemove(args.authorId)
+    
+    // also delete asscociated books for this author
+    await Book.remove({author:{id:args.authorId}})
+
+    return deletedAuthor;
+};
+
+export default deleteAuthorMutation;
