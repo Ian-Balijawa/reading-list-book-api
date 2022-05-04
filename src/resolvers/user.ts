fa858a87a9f60ed39501a9bc01eec7f63715b5cb
ian <ianbalijawa16@gmail.com>
2022-05-04 16:04:32 +0300

    create a resolver to resolve a single user

---
+++ src/resolvers/user.ts
@@ -0,0 +1,26 @@
+import User, { UserDoc } from "../models/User";
+
+type Args = {
+    email: string,
+    name:string,
+};
+
+const userResolver = async (parent: unknown, args: Args): Promise<UserDoc> => {
+    
+    const { email, name } = args;
+
+    if (name && email) {
+        const user = await User.findOne({ email, name });
+        return user;
+    }
+    if (email && !name) {
+        const user = await User.findOne({ email });
+        return user;
+    }
+    if (name && !email) {
+        const user = await User.findOne({ name });
+        return user;
+    }
+};
+
+export default userResolver;
