4c2ec108765ca702ac2529d944e203cd3297e897
ian <ianbalijawa16@gmail.com>
2022-05-04 16:14:13 +0300

    (DB:setup) setup and connected to mongo instance

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import { ApolloServer } from 'apollo-server';
 import resolvers from './resolvers';
 import typeDefs from './schema/schema';
 
+import mongoose from 'mongoose';
+
+const mongoURI = 'mongodb://localhost:27017/reading-list';
+
+mongoose.connect(mongoURI);
+
+mongoose.connection.once('open', () => {
+    console.log('connected to monogDB at :', mongoURI)
+})
+
 const server = new ApolloServer({
     typeDefs: typeDefs,
     resolvers,
