8596bf1f09ed324e26f9f60bdda401c84b0a6133
ian <ianbalijawa16@gmail.com>
2022-05-04 16:06:29 +0300

    create mutation to add an author

---
+++ src/mutations/author/add.ts
@@ -0,0 +1,16 @@
+import Author, { AuthorDoc } from "../../models/Author";
+
+type Args = {
+    name: string;
+    age: number;
+};
+
+const addAuthorMutation = async (parent: unknown, args: Args): Promise<AuthorDoc> => {
+    
+    let newAuthor = Author.build({ name: args.name, age: args.age })
+    
+    newAuthor = await newAuthor.save();
+    return newAuthor
+};
+
+export default addAuthorMutation;
