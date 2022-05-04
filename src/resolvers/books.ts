e6c5cfb98036d333dc6b9ed94f115766a286e731
ian <ianbalijawa16@gmail.com>
2022-05-04 16:04:02 +0300

    create a resolver to resolve multiple books

---
+++ src/resolvers/books.ts
@@ -0,0 +1,19 @@
+import { AuthorDoc } from '../models/Author';
+import Book, { BookDoc } from './../models/Book';
+
+type Args = {
+	name: string;
+	genre: string;
+	author: AuthorDoc;
+};
+
+const booksResolver = async (
+	_parent: unknown,
+	args: Args
+): Promise<Omit<BookDoc & { _id: any; }, never>[]> => {
+	const books = await Book.find({}).populate('Author');
+
+	return books;
+};
+
+export default booksResolver;
