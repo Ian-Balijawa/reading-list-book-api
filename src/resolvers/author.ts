6e57f0cc2d530e06e156e8440a1ea87e4aeae08e
ian <ianbalijawa16@gmail.com>
2022-05-04 16:02:26 +0300

    create a single author resolver

---
+++ src/resolvers/author.ts
@@ -0,0 +1,14 @@
+import Author, {AuthorDoc } from "../models/Author";
+
+type Args = {
+    id: string;
+};
+
+
+const authorResolver = async (parent: unknown, args: Args):Promise<AuthorDoc> => {
+    const author = await Author.findById(args.id);
+    return author;
+};
+
+
+export default authorResolver;
